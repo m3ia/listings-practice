1f0109b4e5f0390ca829948a56bda65a2da73a0a
Melissa Natividad <meianatividad@gmail.com>
2022-02-10 16:17:20 -0800

    Add listing mock data

---
+++ src/components/listings/ListingsBody.js
@@ -0,0 +1,21 @@
+import { listings } from "../../mockData";
+
+function ListingsBody() {
+    // Example property
+    // const property1 = 
+    <listings />
+    return (
+    <div className="Listings-container">
+      <header>
+        <p>
+          <code>Here is the Listings Body</code>
+        </p>
+      </header>
+      <body>
+
+      </body>
+    </div>
+  );
+}
+
+export default ListingsBody;
