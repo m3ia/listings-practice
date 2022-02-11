06fb907e3de66ef356cb422a4dcaad83c86ce7a8
Melissa Natividad <meianatividad@gmail.com>
2022-02-10 18:28:16 -0800

    Create table for listings

--- src/components/listings/ListingsBody.js
+++ src/components/listings/ListingsBody.js
@@ -1,5 +1,18 @@
 import { listings } from "../../mockData";
 import '../../App.css';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(property, unitType, avgSqFt, range) {
+  return { property, unitType, avgSqFt, range };
+}
+
+const listingsData = listings.sort((a, b) => a.name > b.name ? 1: -1);
 
 function ListingsBody() {
     return (
@@ -9,8 +22,19 @@ function ListingsBody() {
           <code>Here is the Listings Body</code>
         </p>
       </header>
-      <body className="Listings-body">
-        {listings.sort((a, b) => a.name > b.name ? 1: -1).map((listing, key) => {
+      <body className="Listings-table">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+                <TableCell>Property</TableCell>
+                <TableCell align="right">Unit&nbsp;Type</TableCell>
+                <TableCell align="right">Avg&nbsp;Square&nbsp;Footage</TableCell>
+                <TableCell align="right">Range</TableCell>
+              </TableRow>
+          </TableHead>
+          <TableBody>
+          {/* {listingsData.map((listing, key) => {
           return (
             <div key={key}>
               {listing.name}
@@ -18,7 +42,25 @@ function ListingsBody() {
               <img src={listing.picture} />
             </div>
           );
-        })}
+        })} */}
+          {listingsData.map((listing) => (
+            <TableRow
+              key={listing.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="left">
+                {listing.name}
+                <br />
+                <img src={listing.picture} />
+                </TableCell>
+              <TableCell align="left">Unit Type Here</TableCell>
+              <TableCell align="left">Avg sq fooage Here</TableCell>
+              <TableCell align="left">Range here</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        </Table>
+      </TableContainer>
       </body>
     </div>
   );
