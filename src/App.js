0fa966fa5969afe566dd7da4d60ea192a9c8945e
Melissa Natividad <meianatividad@gmail.com>
2022-03-02 11:55:01 -0800

    Add amenities strategy

--- src/App.js
+++ src/App.js
@@ -31,6 +31,31 @@ function App() {
       .includes(searchTerm.toLowerCase()))
   .sort((a, b) => a.name > b.name ? 1: -1);
 
+  // Amenities filter
+  // look for all selected amenities in the filter 
+  // Return properties that match those amenities
+  // listingsData.filter(listing => {
+  //   if (searchTerm && !doesListingNameMatch(listing, searchTerm)) {
+  //     return false;
+  //   } 
+
+  //   if (unitAmenities.length > 0 && !doesListingHaveAmenitie(listing, unitAmenities)) {
+  //     return false;
+  //   } 
+
+  //   // ......
+
+  //   return true;
+  // })
+
+  // create a temporary boolean to determine if the listing needs to be filtered (true = show this property; false = filter this property out)
+  // length of the preferred amenities === length of each property's unit amenities? if true
+    // create a Set for the unit amenities
+    // for each preferred amenity, loop through the unit amenities to see if it exists. 
+        // if true, check the next one
+        // if we're at the last item and they're equal, then return true
+    // if true, then 
+
   const { numberOfPages, firstListingIndex, lastListingIndex } = getPaginationInfo(listingsData, listingsPerPage, currentPage);
   return (
     <div className="App">
