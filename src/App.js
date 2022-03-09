ccea74b163f13d0708967534786272fd88c10770
Melissa Natividad <meianatividad@gmail.com>
2022-03-09 11:53:46 -0800

    install Prettier, eslint ext, husky

--- src/App.js
+++ src/App.js
@@ -1,61 +1,70 @@
-import { useState } from 'react';
-import Banner from './components/banner/Banner';
-import ListingsBody from './components/listings/ListingsBody';
-import { listings } from '../src/mockData';
-import './App.css';
+import { useState } from "react";
+import Banner from "./components/banner/Banner";
+import ListingsBody from "./components/listings/ListingsBody";
+import { listings } from "../src/mockData";
+import "./App.css";
 
-
- // For Pagination
- export const getPaginationInfo = (listingsData, listingsPerPage, currentPage) => {
+// For Pagination
+export const getPaginationInfo = (
+  listingsData,
+  listingsPerPage,
+  currentPage
+) => {
   const numberOfListings = listingsData.length;
-  const numberOfPages = Math.ceil(numberOfListings/listingsPerPage);
+  const numberOfPages = Math.ceil(numberOfListings / listingsPerPage);
   const firstListingIndex = (currentPage - 1) * listingsPerPage;
-  const lastListingIndex = (firstListingIndex + listingsPerPage);
+  const lastListingIndex = firstListingIndex + listingsPerPage;
 
-  return {numberOfListings, numberOfPages, firstListingIndex, lastListingIndex};
- }
+  return {
+    numberOfListings,
+    numberOfPages,
+    firstListingIndex,
+    lastListingIndex,
+  };
+};
 
 function App() {
   // For Property Name search filter
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   // For Pagination
   const [listingsPerPage, setListingsPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
-  
-  const listingsData = 
-  searchTerm === "" ? 
-  listings.sort((a, b) => a.name > b.name ? 1: -1) : 
-  listings.filter(
-    ({name}) => name
-      .toLowerCase()
-      .includes(searchTerm.toLowerCase()))
-  .sort((a, b) => a.name > b.name ? 1: -1);
+
+  const listingsData =
+    searchTerm === ""
+      ? listings.sort((a, b) => (a.name > b.name ? 1 : -1))
+      : listings
+          .filter(({ name }) =>
+            name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .sort((a, b) => (a.name > b.name ? 1 : -1));
 
   // Amenities Dropdown Filter
   // An array of all Amenities, for Amenities Filter
-  // Creates a Set of all unique, possible amenities 
+  // Creates a Set of all unique, possible amenities
   const allAmenitiesSet = new Set([]);
   // Loops through all listings to access units...
-  for (let i=0; i < listingsData.length; i++) {
+  for (let i = 0; i < listingsData.length; i++) {
     // Loops through all units to access amenities...
     const listing = listingsData[i];
     const units = listing.units;
-    for (let j=0; j < units.length; j++) {
+    for (let j = 0; j < units.length; j++) {
       const amenities = units[j].amenities;
       // Loops through each amenity to be added to Set
-      for (let k=0; k < amenities.length; k++) {
+      for (let k = 0; k < amenities.length; k++) {
         const amenity = amenities[k];
         allAmenitiesSet.add(amenity);
       }
     }
   }
   // Converts the Set to an array, via https://www.geeksforgeeks.org/how-to-convert-set-to-array-in-javascript/
-  const allAmenitiesArr = Array.from(allAmenitiesSet); 
+  const allAmenitiesArr = Array.from(allAmenitiesSet);
 
-  const { numberOfPages, firstListingIndex, lastListingIndex } = getPaginationInfo(listingsData, listingsPerPage, currentPage);
+  const { numberOfPages, firstListingIndex, lastListingIndex } =
+    getPaginationInfo(listingsData, listingsPerPage, currentPage);
   return (
     <div className="App">
-      <Banner 
+      <Banner
         listingsData={listingsData}
         setSearchTerm={setSearchTerm}
         listingsPerPage={listingsPerPage}
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,131 +1,161 @@
-import '../../App.css';
+import "../../App.css";
 
 // For Amenities Dropdown Filter
-const AmenitiesDropdown = ({allAmenitiesArr}) => {
-    return (
-        <div className="amenities-dropdown-filter">
-            {/*  Amenities Selection Box  */}
-            <div className="selected-amenities border-solid border-2 border-green-500">
-                    <span className="amenity-button rounded-full-auto bg-green-500"> Hi there.  
-                        <button className="amenities-x text-slate-50 font-bold"> x </button>
-                    </span>
-                    <span className="amenity-button rounded-full-auto bg-green-500">Hellloooo
-                        <button className="amenities-x text-slate-50 font-bold"> x </button>
-                    </span>
-                    <span className="amenity-button rounded-full-auto bg-green-500">owie
-                        <button className="amenities-x text-slate-50 font-bold"> x </button>
-                    </span>
-                    <span className="amenity-button rounded-full-auto bg-green-500">Hellloooo
-                        <button className="amenities-x text-slate-50 font-bold"> x </button>
-                    </span>
-                    <span className="amenity-button rounded-full-auto bg-green-500">Hellloooo
-                        <button className="amenities-x text-slate-50 font-bold"> x </button>
-                    </span>
-                    <span className="amenity-button rounded-full-auto bg-green-500"> Hi there.  
-                        <button className="amenities-x text-slate-50 font-bold"> x </button>
-                    </span>
-                    <span className="amenity-button rounded-full-auto bg-green-500">Hellloooo
-                        <button className="amenities-x text-slate-50 font-bold"> x </button>
-                    </span>
-                    <span className="amenity-button rounded-full-auto bg-green-500">owie
-                        <button className="amenities-x text-slate-50 font-bold"> x </button>
-                    </span>
-            </div>
-            {/* Dropdown multi-select list of all unique amenities */}
-            <select multiple className="amenities-dropdown border-solid border-2 border-green-500" >
-                {allAmenitiesArr.map((amenity) => (
-                    <option key={amenity}>{amenity}</option>
-                ))}
-            </select>
-        </div>
-    )
-}
-// For Property Name Filter
-const PropertyNameFilter = ({setSearchTerm}) => {
-    return (
-        <div>
-            <input
-            className=
-            "placeholder:italic placeholder:text-slate-400 block bg-white w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-            type="text"
-            placeholder="Search by property name"
-            onChange={event => {setSearchTerm(event.target.value)}} 
-            />
+const AmenitiesDropdown = ({ allAmenitiesArr }) => {
+  return (
+    <div className="amenities-dropdown-filter">
+      {/*  Amenities Selection Box  */}
+      <div className="selected-amenities border-solid border-2 border-green-500">
+        <span className="amenity-button rounded-full-auto bg-green-500">
+          {" "}
+          Hi there.
+          <button className="amenities-x text-slate-50 font-bold"> x </button>
+        </span>
+        <span className="amenity-button rounded-full-auto bg-green-500">
+          Hellloooo
+          <button className="amenities-x text-slate-50 font-bold"> x </button>
+        </span>
+        <span className="amenity-button rounded-full-auto bg-green-500">
+          owie
+          <button className="amenities-x text-slate-50 font-bold"> x </button>
+        </span>
+        <span className="amenity-button rounded-full-auto bg-green-500">
+          Hellloooo
+          <button className="amenities-x text-slate-50 font-bold"> x </button>
+        </span>
+        <span className="amenity-button rounded-full-auto bg-green-500">
+          Hellloooo
+          <button className="amenities-x text-slate-50 font-bold"> x </button>
+        </span>
+        <span className="amenity-button rounded-full-auto bg-green-500">
+          {" "}
+          Hi there.
+          <button className="amenities-x text-slate-50 font-bold"> x </button>
+        </span>
+        <span className="amenity-button rounded-full-auto bg-green-500">
+          Hellloooo
+          <button className="amenities-x text-slate-50 font-bold"> x </button>
+        </span>
+        <span className="amenity-button rounded-full-auto bg-green-500">
+          owie
+          <button className="amenities-x text-slate-50 font-bold"> x </button>
+        </span>
       </div>
-    );
-}
+      {/* Dropdown multi-select list of all unique amenities */}
+      <select
+        multiple
+        className="amenities-dropdown border-solid border-2 border-green-500"
+      >
+        {allAmenitiesArr.map((amenity) => (
+          <option key={amenity}>{amenity}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+// For Property Name Filter
+const PropertyNameFilter = ({ setSearchTerm }) => {
+  return (
+    <div>
+      <input
+        className="placeholder:italic placeholder:text-slate-400 block bg-white w-fit border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+        type="text"
+        placeholder="Search by property name"
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+    </div>
+  );
+};
 
 // For Pagination
-const PaginationStation = ({ listingsPerPage, setListingsPerPage, currentPage, setCurrentPage, numberOfPages}) => {
-    return (
-        <>
-            <div>
-                <div className="pages-counter">
-                    Page {currentPage} / {numberOfPages}
-                    <br />
-                    <div className="pagination-button">
-                        {currentPage > 1 && (
-                            <button className="rounded bg-orange-100 border-2 border-r-orange-500
+const PaginationStation = ({
+  listingsPerPage,
+  setListingsPerPage,
+  currentPage,
+  setCurrentPage,
+  numberOfPages,
+}) => {
+  return (
+    <>
+      <div>
+        <div className="pages-counter">
+          Page {currentPage} / {numberOfPages}
+          <br />
+          <div className="pagination-button">
+            {currentPage > 1 && (
+              <button
+                className="rounded bg-orange-100 border-2 border-r-orange-500
                             border-b-orange-500
                             "
-                            onClick={() => setCurrentPage(currentPage-1)}
-                            >
-                                Previous Page
-                            </button>
-                        )} {' '}
-                        {currentPage < numberOfPages && (
-                            <button className="rounded bg-orange-100 border-2 border-r-orange-500
+                onClick={() => setCurrentPage(currentPage - 1)}
+              >
+                Previous Page
+              </button>
+            )}{" "}
+            {currentPage < numberOfPages && (
+              <button
+                className="rounded bg-orange-100 border-2 border-r-orange-500
                             border-b-orange-500
                             "
-                            onClick={() => setCurrentPage(currentPage+1)}
-                            >
-                                Next Page
-                            </button>
-                        )
-                        }
+                onClick={() => setCurrentPage(currentPage + 1)}
+              >
+                Next Page
+              </button>
+            )}
+          </div>
+          <div className="number-listings-settings">
+            <p>
+              Show{" "}
+              {[10, 20, 30].map((n) =>
+                n !== listingsPerPage ? (
+                  <a
+                    key={n}
+                    href="#"
+                    onClick={() => {
+                      setListingsPerPage(n);
+                      setCurrentPage(1);
+                    }}
+                  >
+                    <u>{n} </u>
+                  </a>
+                ) : (
+                  `${n} `
+                )
+              )}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-                    </div>
-                    <div className="number-listings-settings">
-                        <p>
-                            Show {' '}
-                            {[10, 20, 30].map((n) => (
-                                n !== listingsPerPage ? (
-                                    <a 
-                                        key={n}
-                                        href ="#" 
-                                        onClick={() => {
-                                            setListingsPerPage(n);
-                                            setCurrentPage(1);
-                                        }
-                                            }>
-                                        <u>{n} {' '}</u>
-                                    </a>
-                                ) : `${n} ` 
-                            ))}
-                        </p>
-                    </div>  
-                </div>
-            </div>
-        </>
-    )
-}
-
-function Banner({setSearchTerm, currentPage, numberOfPages, setCurrentPage, setListingsPerPage, listingsPerPage, allAmenitiesArr}) {
+function Banner({
+  setSearchTerm,
+  currentPage,
+  numberOfPages,
+  setCurrentPage,
+  setListingsPerPage,
+  listingsPerPage,
+  allAmenitiesArr,
+}) {
   return (
     <div className="Banner-container border border-orange-500">
       <div className="amenities-dropdown-filter">
-          <AmenitiesDropdown allAmenitiesArr={allAmenitiesArr} />
+        <AmenitiesDropdown allAmenitiesArr={allAmenitiesArr} />
       </div>
       <div className="property-name-filter">
         <PropertyNameFilter setSearchTerm={setSearchTerm} />
       </div>
       <div className="pagination-station">
-        <PaginationStation 
-            currentPage={currentPage}
-            numberOfPages={numberOfPages}
-            setCurrentPage={setCurrentPage}
-            setListingsPerPage={setListingsPerPage}
-            listingsPerPage={listingsPerPage}
+        <PaginationStation
+          currentPage={currentPage}
+          numberOfPages={numberOfPages}
+          setCurrentPage={setCurrentPage}
+          setListingsPerPage={setListingsPerPage}
+          listingsPerPage={listingsPerPage}
         />
       </div>
     </div>
